60d1d55182691f87849b62e4812311284417ec4d
Vikram Pandit <55878873+vkpandit1137@users.noreply.github.com>
2023-09-01 17:53:13 +0530

    Update login.js

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 let app = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("/opt/render/project/src/models/user.js");
+const User = require("../../Models/User");
 
 // ENVIRONMENT VARIABLES
 require("dotenv").config();
