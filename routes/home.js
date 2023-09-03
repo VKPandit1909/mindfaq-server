52bff21d4e14da7f56692bb7817d95a9be8f7e53
Vikram Pandit <55878873+vkpandit1137@users.noreply.github.com>
2023-09-03 10:30:17 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require('express');
 let app = express.Router();
 
 app.get('/', function (req, res) {
-    res.send('Home');
+    res.send('Home!');
 });
 // app.get('/:id', function (req, res) {
 //     res.send('View Blogs' + req.params.id);
